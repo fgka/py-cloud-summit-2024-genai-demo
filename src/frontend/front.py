6b6ff34a5d9ccabfa40b15f520072b8b865b2aab
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2024-05-27 16:56:11 +0200

    From Gemini

--- src/frontend/front.py
+++ src/frontend/front.py
@@ -6,7 +6,7 @@ import os
 import datetime
 import time
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-from flask_wtf import CSRFProtect
+from flask_wtf import FlaskForm, CSRFProtect
 import requests
 import dateutil.relativedelta
 
@@ -22,24 +22,30 @@ app.config['WTF_CSRF_ENABLED'] = True
 
 csrf = CSRFProtect(app)
 
+class MyForm(FlaskForm):
+    pass
+
 
 @app.route('/')
 def main():
+    form = MyForm()
     """ Retrieve a list of messages from the backend, and use them to render the HTML template """
     response = requests.get(app.config["BACKEND_URI"], timeout=3)
     json_response = json.loads(response.text)
-    return render_template('home.html', messages=json_response)
+    return render_template('home.html', form=form, messages=json_response)
 
 @app.route('/post', methods=['POST'])
 def post():
     """ Send the new message to the backend and redirect to the homepage """
-    new_message = {'author': request.form['name'],
-                   'message':  request.form['message']}
-    requests.post(url=app.config["BACKEND_URI"],
-                  data=jsonify(new_message).data,
-                  headers={'content-type': 'application/json'},
-                  timeout=3)
-    return redirect(url_for('main'))
+    form = MyForm()
+    if form.validate_on_submit():
+        new_message = {'author': request.form['name'],
+                    'message':  request.form['message']}
+        requests.post(url=app.config["BACKEND_URI"],
+                    data=jsonify(new_message).data,
+                    headers={'content-type': 'application/json'},
+                    timeout=3)
+        return redirect(url_for('main'))
 
 def format_duration(timestamp):
     """ Format the time since the input timestamp in a human readable way """
