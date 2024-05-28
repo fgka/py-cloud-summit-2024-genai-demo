b524a9368c0c71057ee619892c903be77ee13a09
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2024-05-28 13:51:06 +0200

    Removed CSRF code

--- src/frontend/front.py
+++ src/frontend/front.py
@@ -6,7 +6,6 @@ import os
 import datetime
 import time
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-from flask_wtf import FlaskForm, CSRFProtect
 import requests
 import dateutil.relativedelta
 
@@ -16,38 +15,28 @@ DEFAULT_PORT = 8123
 PORT = os.environ.get('PORT', DEFAULT_PORT)
 DEFAULT_HOST = "127.0.0.1"
 HOST = os.environ.get('HOST', DEFAULT_HOST)
-SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
 app.config["BACKEND_URI"] = f'http://{GUESTBOOK_API_ADDR}/messages'
-app.config['WTF_CSRF_ENABLED'] = True
-csrf = CSRFProtect(app)
-
-class MyForm(FlaskForm):
-    pass
 
 
 @app.route('/')
 def main():
     """ Retrieve a list of messages from the backend, and use them to render the HTML template """
-    form = MyForm()
     response = requests.get(app.config["BACKEND_URI"], timeout=3)
     json_response = json.loads(response.text)
-    return render_template('home.html', form=form, messages=json_response)
+    return render_template('home.html', messages=json_response)
 
 @app.route('/post', methods=['POST'])
 def post():
     """ Send the new message to the backend and redirect to the homepage """
-    form = MyForm()
-    if form.validate_on_submit():
-        new_message = {'author': request.form['name'],
-                    'message':  request.form['message']}
-        requests.post(url=app.config["BACKEND_URI"],
-                    data=jsonify(new_message).data,
-                    headers={'content-type': 'application/json'},
-                    timeout=3)
-        return redirect(url_for('main'))
+    new_message = {'author': request.form['name'],
+                'message':  request.form['message']}
+    requests.post(url=app.config["BACKEND_URI"],
+                data=jsonify(new_message).data,
+                headers={'content-type': 'application/json'},
+                timeout=3)
+    return redirect(url_for('main'))
 
 def format_duration(timestamp):
     """ Format the time since the input timestamp in a human readable way """
