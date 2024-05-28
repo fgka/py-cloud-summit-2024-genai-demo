33a8162473390879044dedecec30553126efce62
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2024-05-28 14:11:07 +0200

    Removed refacting of back.py

--- src/backend/back.py
+++ src/backend/back.py
@@ -7,7 +7,6 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import bleach
 
-# Configuration
 DEFAULT_GUESTBOOK_DB_ADDR = "127.0.0.1:27017"
 GUESTBOOK_DB_ADDR = os.environ.get('GUESTBOOK_DB_ADDR', DEFAULT_GUESTBOOK_DB_ADDR)
 DEFAULT_PORT = 8321
@@ -15,40 +14,27 @@ PORT = os.environ.get('PORT', DEFAULT_PORT)
 DEFAULT_HOST = "127.0.0.1"
 HOST = os.environ.get('HOST', DEFAULT_HOST)
 
-# Initialize Flask app
 app = Flask(__name__)
 app.config["MONGO_URI"] = f'mongodb://{GUESTBOOK_DB_ADDR}/guestbook'
 mongo = PyMongo(app)
 
-# Define helper functions
-def clean_message(message):
-    """Sanitizes the message content using bleach.clean to prevent XSS attacks."""
-    return bleach.clean(message)
-
-def create_message_data(author, message):
-    """Creates a dictionary with the author, message, and timestamp."""
-    return {'author': clean_message(author),
-            'message': clean_message(message),
-            'date': time.time()}
-
-# Define routes
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    """Retrieves and returns a list of messages from the database."""
+    """ retrieve and return the list of messages on GET request """    
     field_mask = {'author':1, 'message':1, 'date':1, '_id':0}
     msg_list = list(mongo.db.messages.find({}, field_mask).sort("_id", -1))
     return jsonify(msg_list), 201
 
 @app.route('/messages', methods=['POST'])
 def add_message():
-    """Saves a new message to the database."""
+    """ save a new message on POST request """
     raw_data = request.get_json()
-    msg_data = create_message_data(raw_data['author'], raw_data['message'])
+    msg_data = {'author':bleach.clean(raw_data['author']),
+                'message':bleach.clean(raw_data['message']),
+                'date':time.time()}
     mongo.db.messages.insert_one(msg_data)
-    return jsonify({}), 201
+    return  jsonify({}), 201
 
-# Main execution
 if __name__ == '__main__':
-    # Start Flask server
-    # Flask's debug mode is unrelated to ptvsd debugger used by Cloud Code
+    # start Flask server
     app.run(debug=False, port=PORT, host=HOST)
